b05339819a635c42aaea7fd0a81f46511a83470e
Jeff Hanson <jeffrey.hanson@uqconnect.edu.au>
2015-06-24 14:45:22 +1000

    elegant admin funcitons

--- run.R
+++ run.R
@@ -1,7 +1,5 @@
 ## Run locally
-test=function(){shiny::runApp("C:/Users/jhanson/Documents/GitHub/data-viewer-biol2015")}
-test=function(){shiny::runApp("C:/Users/jeff/Documents/GitHub/data-viewer-biol2015")}
-test=function(){shiny::runApp("/home/jeff/Github/data-viewer-biol2015")}
+test=function(){shiny::runApp(file.path(Sys.getenv('HOME'), 'github', 'data-viewer-biol2015'))}
 
 ## Deploy online to ShinyApps
 shinyapps::deployApp("C:/Users/jeff/Documents/GitHub/data-viewer-biol2015")
